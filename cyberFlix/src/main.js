d1f5193b224398a73fdfe76b09520b497152d923
antoniorony <antoniorony2013@gmail.com>
2020-07-02 10:45:05 -0300

    AULA 34 - SISTEMA DE ROTAS VUE-ROUTER

--- cyberFlix/src/main.js
+++ cyberFlix/src/main.js
@@ -1,28 +1,26 @@
 import Vue from 'vue'
 //import App from './App.vue'
+import VueRouter from 'vue-router'
 
 import VueResource from 'vue-resource'
 Vue.use(VueResource)
+Vue.use(VueRouter)
 
 import Home from './paginas/Home.vue';
 const Contato = {template: '<p>Página de Contato</p>'}
 const SemPagina = {template: '<h1>Pagina não existe</h1>'}
 
-const rotas = {
-  '/': Home,
-  '/contato': Contato,
-}
+const routes = [
+  {path: '/', component: Home},
+  {path: '/contato', component: Contato}
+]
 
-new Vue({
-  el: '#app',
-  data:{
-    rotaAtual: window.location.pathname
-  },
-  computed: {
-    exibeComponente(){
-      return rotas[this.rotaAtual] || SemPagina;
-    }
-  },
-
-  render (h) { return h(this.exibeComponente) }
+const router = new VueRouter({
+  routes
 })
+
+
+
+const app = new Vue({
+  router
+}).$mount('#app')
