4652ad9deef11f264f66ffb1298a927782028ab1
antoniorony <antoniorony2013@gmail.com>
2020-07-02 09:59:30 -0300

    AULA 33 - MODELO SIMPLES DE ROTAS

--- cyberFlix/src/main.js
+++ cyberFlix/src/main.js
@@ -1,10 +1,28 @@
 import Vue from 'vue'
-import App from './App.vue'
-import VueResource from 'vue-resource'
+//import App from './App.vue'
 
+import VueResource from 'vue-resource'
 Vue.use(VueResource)
+
+import Home from './paginas/Home.vue';
+const Contato = {template: '<p>Página de Contato</p>'}
+const SemPagina = {template: '<h1>Pagina não existe</h1>'}
+
+const rotas = {
+  '/': Home,
+  '/contato': Contato,
+}
 
 new Vue({
   el: '#app',
-  render: h => h(App)
+  data:{
+    rotaAtual: window.location.pathname
+  },
+  computed: {
+    exibeComponente(){
+      return rotas[this.rotaAtual] || SemPagina;
+    }
+  },
+
+  render (h) { return h(this.exibeComponente) }
 })
